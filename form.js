c901b09af3fe593f924942d5fc906b5517f335ce
Hannah Campbell (Troyer) <84107418+Hmcampbell2911@users.noreply.github.com>
2021-09-13 19:31:17 -0400

    Initial commit
    Initial commit

---
+++ form.js
@@ -0,0 +1,30 @@
+function myFunction1() {
+    var x = document.getElementById("MedSynchronization");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function myFunction2() {
+var x = document.getElementById("Immunizations");
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+function myFunction3() {
+var x = document.getElementById("OTC");
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
+function myFunction4() {
+    var x = document.getElementById("MTM");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";}}
